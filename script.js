f4681c04388215950f6205ab054f5a16c8b6b44f
Nailgy <ilyha.nali@gmail.com>
2022-07-20 04:32:59 +0300

    Add funcs to edit text and add/remove last digits

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const changeText = (element, newText) => {
+  element.textContent = newText;
+};
+
+const clearText = (element) => {
+  changeText(element, '0');
+};
+
+const addDigit = (digit, num) => {
+  const strNum = num.toString();
+  const newStrNum = strNum.concat(digit);
+  const res = Number(newStrNum);
+  return res;
+};
+
+const removeLastDigit = (num) => {
+  const strNum = num.toString();
+  const newStrNum = strNum.slice(0, -1);
+  const res = Number(newStrNum);
+  return res;
+};
